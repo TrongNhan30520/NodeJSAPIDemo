092aeffa7e055bcc5cb2880fa97ae852abcd2c1a
trongnhan30520 <trongnhan30520@gmail.com>
2021-12-30 21:07:49 +0700

    dd

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const importData = require("./data.json")
+const importData1 = require("./data1.json")
 const app = express()
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello word!")
+    res.send(`<h1>/data & /data1</h1>`)
 })
 
 app.get("/data", (req, res) => {
     res.send(importData)
+})
+
+app.get("/data1", (req, res) => {
+    res.send(importData1)
 })
 
 app.listen(port, ()=> {
